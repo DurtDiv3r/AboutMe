05095e0dfc325832e7bb7b9f5cd2f7211f71d342
DurtDiv3r <durtdev@gmail.com>
2021-07-02 11:22:11 +0200

    Initial Databinding

--- app/src/main/java/com/durtdev/aboutme/MainActivity.kt
+++ app/src/main/java/com/durtdev/aboutme/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.durtdev.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.durtdev.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edittext)
+        val textView = findViewById<TextView>(R.id.nickname_textView)
+
+        binding.apply {
+            nicknameTextView.text = nicknameEdittext.text
+            invalidateAll()
+            nicknameEdittext.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+        }
+
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
